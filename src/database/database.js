613cbcf21bbfa14d85f66ae6c5b1b6cdc730d33a
Laura Tissi <laura.tracierra@gmail.com>
2021-07-01 19:07:12 -0300

    mudei database

--- src/database/database.js
+++ src/database/database.js
@@ -3,7 +3,7 @@ import pg from 'pg';
 const { Pool } = pg;
 
 const connection = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.d94lrfmug95qij,
   ssl: {
       rejectUnauthorized: false
   }
